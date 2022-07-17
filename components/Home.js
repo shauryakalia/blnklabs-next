541c3806c2383359feba6da18e1a0e8d2c48bfc7
ShubhamSahaniNitkkr <shubhamsahaninitkkr@gmail.com>
2022-07-17 07:33:43 +0530

    - modal added

--- components/Home.js
+++ components/Home.js
@@ -4,6 +4,7 @@ import Rellax from "rellax";
 
 export default function Home({ data }) {
   const [_isMobile, setMobile] = useState();
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     if (window.innerWidth < 768) setMobile(isMobile);
@@ -22,15 +23,58 @@ export default function Home({ data }) {
   const { videoUrl, content1, content2, highlightText } = data;
   const [text1, text2] = highlightText.split("h3 ");
 
+  const renderModal = () => {
+    return (
+      <div
+        id="myModal"
+        class="modal"
+        style={{
+          display: showModal ? "block" : "none",
+          height: _isMobile ? "-webkit-fill-available" : `100%`,
+          width: "100%",
+          backgroundImage: _isMobile
+            ? "/assets/Lines/mobile01.png"
+            : "/assets/Lines/windows01.png",
+        }}
+      >
+        <div class="modal-content" style={{ width: _isMobile ? "90%" : "60%" }}>
+          <div class="modal-body">
+            <span class="close" onClick={() => setShowModal(!showModal)}>
+              &times;
+            </span>
+
+            <iframe
+              width="100%"
+              height="395"
+              src={
+                showModal
+                  ? "https://www.youtube.com/embed/prY-Ip2xoSE?autoplay=1"
+                  : ""
+              }
+              title="BLNKLABS"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <>
+      {renderModal()}
       <div id="home" className={` pt-1 ${_isMobile ? "" : "d-flex"}`}>
         <div
           className={`col-md-2 d-flex justify-content-end ${
             _isMobile ? "d-none" : ""
           }`}
         >
-          <i className="bi bi-play-circle homePlayBtn"></i>
+          <i
+            onClick={() => setShowModal(!showModal)}
+            className="bi bi-play-circle homePlayBtn"
+          ></i>
         </div>
         <div
           id="homeVideo"
@@ -48,7 +92,10 @@ export default function Home({ data }) {
           </div>
         </div>
         <div className={`col-md-12 text-center ${_isMobile ? "" : "d-none"}`}>
-          <i className="bi bi-play-circle mobileHomePlayBtn"></i>
+          <i
+            onClick={() => setShowModal(!showModal)}
+            className="bi bi-play-circle mobileHomePlayBtn"
+          ></i>
         </div>
       </div>
       <div className={` h5 text-muted ${_isMobile ? "px-5 pb-5" : "p-5"}`}>
